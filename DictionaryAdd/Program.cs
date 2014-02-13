c042f579a4058ebe90d84918d375fbb11b21bdf3
Michael Weems <tokyodweller@gmail.com>
2014-02-12 19:12:29 -0600

    program files

---
+++ DictionaryAdd/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryAdd
+{
+    class Program
+    {
+        static void Main()
+        {
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();
+            dictionary.Add("cat", 2);
+            dictionary.Add("dog", 1);
+            dictionary.Add("llama", 0);
+            dictionary.Add("iguana", -1);
+        }
+    }
+}
